c89757e9ee03b26517f7949663abbf517710f528
robo19 <jonesk22@ashevilleschool.org>
2018-11-13 16:49:28 -0500

    Kevin test 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sense_IMU_Kevin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sense_IMU_Kevin.java
@@ -68,9 +68,12 @@ public class Sense_IMU_Kevin extends OpMode
             if (new_heading < 0) {
                 new_heading = 360 + new_heading;
             }
-            for(;imu.getHeading() != new_heading;)
+            for(;new_heading - 0.5 >= imu.getHeading() && imu.getHeading() <= new_heading + 0.5;)
                 left.setPower(-0.25);
                 right.setPower(0.25);
+
+            left.setPower(0.0);
+            right.setPower(0.0);
 
 
 
