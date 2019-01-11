77975ac3b909de3d4b395cac4078e6efbe66742a
robo19 <jonesk22@ashevilleschool.org>
2019-01-11 17:00:13 -0500

    Kevin Sync

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldAlignExample_Kevin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldAlignExample_Kevin.java
@@ -129,6 +129,7 @@ public class GoldAlignExample_Kevin extends OpMode
 
         telemetry.addData("IsAligned" , detector.getAligned()); // Is the bot aligned with the gold mineral?
         telemetry.addData("X Pos" , detector.getXPosition()); // Gold X position.
+        telemetry.addData("Power",power);
     }
 
     /*
