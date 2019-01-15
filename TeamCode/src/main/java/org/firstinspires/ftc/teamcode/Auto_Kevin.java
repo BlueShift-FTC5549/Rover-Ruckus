ca468626f27f927a899ca74eced3a265a16d813a
robo19 <jonesk22@ashevilleschool.org>
2019-01-15 15:56:19 -0500

    Kevin Sync

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Kevin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Kevin.java
@@ -1,48 +1,19 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
+import com.blueshiftrobotics.ftc.AutoFourWheelDrive;
 import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.DogeCV;
 import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.sun.tools.javac.tree.DCTree;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Autonomous(name="Sky Net", group="Main")
+public class Auto_Kevin extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private AutoFourWheelDrive autoFourWheelDrive;
 
-@TeleOp(name="GoldAlign Example Kevin", group="DogeCV")
-
-public class GoldAlignExample_Kevin extends OpMode
-{
-    // Detector object
     private GoldAlignDetector detector;
     private DcMotor motorDriveLeftBack;
     private DcMotor motorDriveLeftFront;
@@ -52,7 +23,9 @@ public class GoldAlignExample_Kevin extends OpMode
     double power;
 
     @Override
-    public void init() {
+    public void runOpMode() {
+        //Init
+
         telemetry.addData("Status", "DogeCV 2018.0 - Gold Align Example");
 
         motorDriveLeftBack = hardwareMap.get(DcMotor.class,   "motorDriveLeftBack");
@@ -85,35 +58,14 @@ public class GoldAlignExample_Kevin extends OpMode
         detector.enable(); // Start the detector!
 
 
-    }
+        waitForStart();
 
-    /*
-     * Code to run REPEATEDLY when the driver hits INIT
-     */
-    @Override
-    public void init_loop() {
-    }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
-
-    }
-
-    /*
-     * Code to run REPEATEDLY when the driver hits PLAY
-     */
-    @Override
-    public void loop() {
+        //Loop
         x_pos = detector.getXPosition();
 
         if (x_pos >= 300 && x_pos <= 340) {
-            motorDriveRightBack.setPower(0.7);
-            motorDriveRightFront.setPower(0.7);
-            motorDriveLeftBack.setPower(0.7);
-            motorDriveLeftFront.setPower(0.7);
+            autoFourWheelDrive.encoderDrive(20, 15);
         }
 
         else if (x_pos < 310) {
@@ -138,14 +90,4 @@ public class GoldAlignExample_Kevin extends OpMode
         telemetry.addData("X Pos" , detector.getXPosition()); // Gold X position.
         telemetry.addData("Power ",1.0 - power);
     }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-        // Disable the detector
-        detector.disable();
-    }
-
 }
